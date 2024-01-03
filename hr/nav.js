85f688c1f3675f459d747f9856bd14de69b19405
Filip-Gale <84683869+Filip-Gale@users.noreply.github.com>
2024-01-03 15:29:16 +0100

    Add files via upload

---
+++ hr/nav.js
@@ -0,0 +1,27 @@
+var navigationHTML = '<nav id="nav">' +
+    '    <div id="nav-left-side">' +
+    '        <ul>' +
+    '            <a href="games.html"><li>Igre</li></a>' +
+    '            <a href="gallery.html"><li>Galerija</li></a>' +
+    '            <a href="projects.html"><li>Projekti</li></a>' +
+    '        </ul>' +
+    '    </div>' +
+    '    <a href="index.html">' +
+    '        <img src="../img/logo.png" alt="menu" class="navImg" id="logo">' +
+    '    </a>' +
+    '    <div id="nav-right-side">' +
+    '        <ul>' +
+    '            <a href="contact.html"><li>Kontakt</li></a>' +
+    '            <a href="about.html"><li>O meni</li></a>' +
+    '            <li class="dropdown">' +
+    '                <a>Jezici</a>' +
+    '                <ul class="dropdown-menu">' +
+    '                    <li><a href="../en/index.html">English</a></li>' +
+    '                    <li><a href="#">Hrvatski</a></li>' +
+    '                </ul>' +
+    '            </li>' +
+    '        </ul>' +
+    '    </div>' +
+    '</nav>';
+
+document.write(navigationHTML);
